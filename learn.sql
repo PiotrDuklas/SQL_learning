b502ba9f7612b3a2fcb2cd3e381e1fbe7bd0213f
Piotr Duklas <pedrosson@gmail.com>
2023-03-29 17:02:12 +0200

    add some functions

--- learn.sql
+++ learn.sql
@@ -1,3 +1,55 @@
-//Esencja SQL do nauki
+--Esencja SQL do nauki
 
-Tworzenie bazy danych i tabel
+/*Tworzenie bazy danych i tabel
+
+funkcja describe nazwa_tabeli daje podgląd
+funkcja date_format(nazwa_kolumny którą chcemy zmienić)*/
+
+Aggregate functions - examples:
+• AVG() - Returns the average value
+• STDEV() - Returns the standard deviation value
+• COUNT() - Returns the number of rows
+• MAX() - Returns the largest value
+• MIN() - Returns the smallest value
+• SUM() - Returns the sum
+• etc.
+Scalar functions - examples:
+• UPPER() - Converts a field to upper case
+• LOWER() - Converts a field to lower case
+• LEN() - Returns the length of a text field
+• ROUND() - Rounds a numeric field to the number of decimals specified
+• GETDATE() - Returns the current system date and time
+
+• CHAR
+• CHARINDEX
+• REPLACE
+• SUBSTRING
+• LEN
+• REVERSE
+• LEFT
+• RIGHT
+• LOWER
+• UPPER
+• LTRIM
+• RTRIM
+
+11.1.2 Date and Time Functions
+Here are some useful Date and Time functions in SQL Server:
+• DATEPART
+• GETDATE
+• DATEADD
+• DATEDIFF
+• DAY
+• MONTH
+• YEAR
+• ISDATE
+Read more about these functions in the SQL Server Help.
+11.1.3 Mathematics and Statistics Functions
+Here are some useful functions for mathematics and statistics in SQL Server:
+• COUNT
+• MIN, MAX
+• COS, SIN, TAN
+• SQRT
+• STDEV
+• MEAN
+• AVG
