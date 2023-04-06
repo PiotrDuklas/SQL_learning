068a53e7ef528816725fad6f0b17bfe119ffc0fd
Piotr Duklas <pedrosson@gmail.com>
2023-04-06 11:51:38 +0200

    add some functions and joins

--- learn.sql
+++ learn.sql
@@ -6,28 +6,52 @@ funkcja describe nazwa_tabeli daje podgląd
 funkcja date_format(nazwa_kolumny którą chcemy zmienić)*/
 
 /*Aggregate functions - examples:
-• AVG() - Returns the average value
-• COUNT() - Returns the number of rows
-• MAX() - Returns the largest value
-• MIN() - Returns the smallest value
-• SUM() - Returns the sum
+AVG() - Returns the average value
+COUNT() - Returns the number of rows
+MAX() - Returns the largest value
+MIN() - Returns the smallest value
+SUM() - Returns the sum
+FLOOR() - returns rounds number to floor
+CEILING() - returns rounds number to ceiling
+LEFT('tekst', liczba znaków)
+RIGHT('tekst', liczba znaków)
+UPPER() - Converts a field to upper case
+LOWER() - Converts a field to lower case
+LENGTH() - Returns the length of a text field
+ROUND() - Rounds a numeric field to the number of decimals specified
+NOW() - Returns the current system date and time
+CHARSET() - returns info about data type
 
-• LEFT('tekst', liczba znaków)
-• RIGHT('tekst', liczba znaków)
-• UPPER() - Converts a field to upper case
-• LOWER() - Converts a field to lower case
-• LENGTH() - Returns the length of a text field
-• ROUND() - Rounds a numeric field to the number of decimals specified
-
-• NOW() - Returns the current system date and time
-
-• CHAR
-• CHARINDEX
-• REPLACE
-• SUBSTRING
-• REVERSE
-
-• LTRIM
-• RTRIM
+REPLACE('sobota','o','e') returns string with replaced sings 'o' for 'e'
+SUBSTRING('sobota',3,4) returns a piece of text 'sobota', from third sign, 4 signs
+REVERSE() - returns reversed string
+day(now()) - returns today's number day
+dayname(now()) - returns today's day name
+month(now()) - returns today's number month
+monthname(now()) - returns today's month name
+year(now()) - returns today's number year
+yearweek(now()) - returns today's year and week number
+ADDDATE(NOW(), -30) - returns date 30 days before today
+TRUNCATE(10.55555, 1) - returns value with 1 decimal
 
 
+datediff
+
+CHARINDEX
+LTRIM
+RTRIM
+
+
+
+Inner join lub join - część wspólna z 2 tabel: tcustomers i tsales
+SELECT c.*, s.* from tcustomers c join tsales s on c.customer_id = s.customer_id;
+ 
+Left join - lewa tabela + wspólne z prawej
+SELECT c.*, s.* from tcustomers c left join tsales s on c.customer_id = s.customer_id;
+ 
+Right join - prawa tabela + wspólne z lewej
+SELECT c.*, s.* from tcustomers c right join tsales s on c.customer_id = s.customer_id;
+ 
+Cross join - iloczyn kartezjański
+SELECT c.*, s.* from tcustomers c cross join tsales s
+ 
